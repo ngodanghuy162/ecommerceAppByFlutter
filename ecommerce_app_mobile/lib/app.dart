7e102904a670db92647d9a95068228a8f6048be5
duykhanhxx03 <duykhanhxx03@gmail.com>
2023-11-02 15:58:21 +0700

    code base

---
+++ ecommerce_app_mobile/lib/app.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:ecommerce_app_mobile/utils/constants/text_strings.dart';
+import 'package:ecommerce_app_mobile/utils/theme/theme.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: TTexts.appName,
+      themeMode: ThemeMode.system,
+      theme: TAppTheme.lightTheme,
+      darkTheme: TAppTheme.darkTheme,
+      debugShowCheckedModeBanner: false,
+      // initialBinding: GeneralBindings(),
+      home: const Scaffold(
+        body: Center(
+          child: Text(
+            'Code base',
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
