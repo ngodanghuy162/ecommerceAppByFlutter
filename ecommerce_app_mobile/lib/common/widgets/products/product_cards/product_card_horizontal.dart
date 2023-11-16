a16ea13bf0eda8e81b62751fe57e520e0f745b4c
ngodanghuy162 <ngohuy658@gmail.com>
2023-11-17 06:16:11 +0700

    SubCategoryScreen-29 done

---
+++ ecommerce_app_mobile/lib/common/widgets/products/product_cards/product_card_horizontal.dart
@@ -0,0 +1,130 @@
+import 'dart:js';
+
+import 'package:ecommerce_app_mobile/common/styles/product_price_text.dart';
+import 'package:ecommerce_app_mobile/common/styles/product_title_text.dart';
+import 'package:ecommerce_app_mobile/common/styles/shadows.dart';
+import 'package:ecommerce_app_mobile/common/styles/t_brand_title_text_with_verified_icon.dart';
+import 'package:ecommerce_app_mobile/common/widgets/custom_shapes/container/rounded_container.dart';
+import 'package:ecommerce_app_mobile/common/widgets/icons/t_circular_icon.dart';
+import 'package:ecommerce_app_mobile/common/widgets/images/t_rounded_image.dart';
+import 'package:ecommerce_app_mobile/utils/constants/colors.dart';
+import 'package:ecommerce_app_mobile/utils/constants/image_strings.dart';
+import 'package:ecommerce_app_mobile/utils/constants/sizes.dart';
+import 'package:ecommerce_app_mobile/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class TProductCardHorizontal extends StatelessWidget {
+  const TProductCardHorizontal({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return Container(
+      width: 180,
+      padding: const EdgeInsets.all(1),
+      decoration: BoxDecoration(
+         // boxShadow: [TShadowStyle.verticalProductShadow],
+          borderRadius: BorderRadius.circular(TSizes.productImageRadius),
+          color: dark ? TColors.darkerGrey : TColors.softGrey),
+      child: Row(
+        children: [
+          //thumbnail
+          TRoundedContainer(
+            height: 100,
+            padding: const EdgeInsets.all(TSizes.sm),
+            backgroundColor: dark ? TColors.dark : TColors.light,
+            child: Stack(
+              children: [
+                SizedBox(
+                  height: 120,
+                  width: 120,
+                  child: TRoundedImage(
+                    imageUrl: TImages.productImage1,
+                    applyImageRadius: true,
+                  ),
+                ),
+
+                //sale tag
+                Positioned(
+                  top: 12,
+                  child: TRoundedContainer(
+                    radius: TSizes.sm,
+                    backgroundColor: TColors.secondary.withOpacity(0.8),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: TSizes.sm, vertical: TSizes.xs),
+                    child: Text('25%',
+                        style: Theme.of(context).textTheme.labelLarge!.apply(
+                              color: TColors.black,
+                            )),
+                  ),
+                ),
+                // -- Favourite Button
+                const Positioned(
+                  top: 0,
+                  right: 0,
+                  child: TCircularIcon(
+                    icon: Iconsax.heart5,
+                    color: Colors.red,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            width: 172,
+            child: Padding(
+              padding: const EdgeInsets.only(top: TSizes.sm, left: TSizes.sm),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: TSizes.sm),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TProductTitleText(
+                          title: "Green Nike Half",
+                          smallSize: true,
+                        ),
+                        SizedBox(
+                          height: TSizes.spaceBtwItems / 2,
+                        ),
+                        TBrandTitleWithVerifiedIcon(title: "Nike")
+                      ],
+                    ),
+                  ),
+                  const Spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Flexible(child: TProductPriceText(price: "256.0")),
+                      //add to cart
+                      Container(
+                        decoration: const BoxDecoration(
+                            color: TColors.dark,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(TSizes.cardRadiusMd),
+                              bottomRight:
+                                  Radius.circular(TSizes.productImageRadius),
+                            )),
+                        child: const SizedBox(
+                          width: TSizes.iconLg * 1.2,
+                          height: TSizes.iconLg * 1.2,
+                          child: Center(
+                            child: Icon(
+                              Iconsax.add,
+                              color: TColors.white,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
