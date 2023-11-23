1db501ffa007fd915f54c4a6ec860c538fc87370
unknown <stkey002@gmail.com>
2023-11-23 15:22:39 +0700

    finish statistic screen

--- ecommerce_app_mobile/lib/app.dart
+++ ecommerce_app_mobile/lib/app.dart
@@ -1,4 +1,7 @@
 import 'package:ecommerce_app_mobile/features/authentication/screens/onboarding/onboarding.dart';
+import 'package:ecommerce_app_mobile/features/shop/screens/statistics/shipping_status.dart';
+import 'package:ecommerce_app_mobile/features/shop/screens/statistics/statistics_screen.dart';
+import 'package:ecommerce_app_mobile/features/shop/screens/statistics/widgets/bar_graph.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:ecommerce_app_mobile/utils/constants/text_strings.dart';
---
+++ ecommerce_app_mobile/lib/features/shop/screens/statistics/statistics_screen.dart
@@ -0,0 +1,79 @@
+import 'package:ecommerce_app_mobile/common/widgets/appbar/appbar.dart';
+import 'package:ecommerce_app_mobile/common/widgets/custom_shapes/container/rounded_container.dart';
+import 'package:ecommerce_app_mobile/features/shop/screens/statistics/widgets/annotated_chart.dart';
+import 'package:ecommerce_app_mobile/features/shop/screens/statistics/widgets/dropdown.dart';
+import 'package:ecommerce_app_mobile/features/shop/screens/statistics/widgets/bar_graph.dart';
+import 'package:ecommerce_app_mobile/features/shop/screens/statistics/widgets/productItems.dart';
+import 'package:ecommerce_app_mobile/features/shop/screens/statistics/widgets/statistic_number.dart';
+import 'package:ecommerce_app_mobile/utils/constants/colors.dart';
+import 'package:ecommerce_app_mobile/utils/constants/sizes.dart';
+import 'package:ecommerce_app_mobile/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class StatisticScreen extends StatelessWidget {
+  const StatisticScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return Scaffold(
+      appBar: TAppBar(title: Text("Statistics"), showBackArrow: true),
+      body: Padding(
+        padding: EdgeInsets.all(TSizes.defaultSpace),
+        child: Column(
+          children: [
+            TRoundedContainer(
+                showBorder: true,
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    DropDownMenu(),
+                    Divider(
+                      height: 1,
+                    ),
+                    SizedBox(height: TSizes.spaceBtwSections),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        StatisticNumber(
+                            text: "Total", color: Colors.red, number: 20),
+                        StatisticNumber(
+                            text: "Waiting", color: Colors.black, number: 10),
+                        StatisticNumber(
+                            text: "In Shipping", color: Colors.blue, number: 5),
+                        StatisticNumber(
+                            text: "Success", color: Colors.green, number: 5)
+                      ],
+                    ),
+                  ],
+                )),
+            const SizedBox(height: TSizes.spaceBtwSections),
+            SizedBox(
+              height: 350,
+              child: BarChartGraph(),
+            ),
+            const SizedBox(height: TSizes.spaceBtwSections),
+            const AnnotatedChart(),
+            const SizedBox(height: TSizes.spaceBtwSections),
+            Divider(
+              height: 0.7,
+            ),
+            const SizedBox(height: TSizes.spaceBtwSections),
+            Row(
+              children: [
+                Text(
+                  "Received Money:",
+                  style: TextStyle(
+                      color: Colors.green, fontWeight: FontWeight.bold),
+                ),
+                Text(" \$300")
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
