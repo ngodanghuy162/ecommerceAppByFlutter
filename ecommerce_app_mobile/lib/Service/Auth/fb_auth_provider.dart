c664991ad094503ebec24c3616ff46b96490bf30
ngodanghuy162 <ngohuy658@gmail.com>
2023-11-26 11:00:41 +0700

    Login fb oke

--- ecommerce_app_mobile/lib/Service/Auth/fb_auth_provider.dart
+++ ecommerce_app_mobile/lib/Service/Auth/fb_auth_provider.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:ecommerce_app_mobile/Service/Repository/user_repository.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:ecommerce_app_mobile/Service/Model/user_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -16,53 +17,64 @@ class FbAuthProvider {
   // Hàm khởi tạo internal (chỉ được gọi bởi factory constructor)
   FbAuthProvider._internal();
 
-  Future<UserCredential?> signInWithFacebook() async {
+  Future<UserCredential> signInWithFacebook() async {
     try {
-      // Trigger the sign-in flow
-      final LoginResult loginResult = await FacebookAuth.instance.login();
-
-      if (loginResult.status == LoginStatus.success) {
-        // Create a credential from the access token (
-        // dang dangloi dang loi o day bi null nhe);
-        final OAuthCredential facebookAuthCredential =
-            FacebookAuthProvider.credential(loginResult.accessToken!.token);
+      if (kIsWeb) {
+        FacebookAuthProvider facebookProvider = FacebookAuthProvider();
+        facebookProvider.addScope('email');
+        facebookProvider.setCustomParameters({
+          'display': 'popup',
+        });
 
         // Once signed in, return the UserCredential
-        UserCredential? userCredential = await FirebaseAuth.instance
-            .signInWithCredential(facebookAuthCredential);
+        return await FirebaseAuth.instance.signInWithPopup(facebookProvider);
+      } else {
+        // Trigger the sign-in flow
+        final LoginResult loginResult = await FacebookAuth.instance.login();
 
-        User? authUser = userCredential.user;
-        String? emailUser = authUser!.email;
-        String? phoneNumber = authUser.phoneNumber;
-        CloudUserModel? currentCloudUser2;
-        CloudUserModel? currentCloudUser = await UserRepository.userRepository
-            .getCloudUserByEmail(emailUser.toString());
-        log(currentCloudUser.toString());
-        if (phoneNumber != null && emailUser == null) {
-          currentCloudUser2 = await UserRepository.userRepository
-              .getCloudUserByPhoneNumber(authUser.phoneNumber.toString());
-        }
-        if (currentCloudUser == null && currentCloudUser2 == null) {
-          currentCloudUser = await UserRepository.userRepository
-              .getCloudUserByPhoneNumber(authUser.phoneNumber.toString());
-          // Kiểm tra và gán giá trị mặc định nếu là null
-          String userName = authUser.displayName ?? 'Unknown';
-          String userEmail = authUser.email ?? 'Unknown';
+        if (loginResult.status == LoginStatus.success) {
+          // Create a credential from the access token (
+          // dang dangloi dang loi o day bi null nhe);
+          final OAuthCredential facebookAuthCredential =
+              FacebookAuthProvider.credential(loginResult.accessToken!.token);
 
-          await UserRepository.userRepository.createUser(
-            CloudUserModel.registerByGg(
-              userId: authUser.uid,
-              userName: userName,
-              email: userEmail,
-            ),
+          // Once signed in, return the UserCredential
+          UserCredential? userCredential = await FirebaseAuth.instance
+              .signInWithCredential(facebookAuthCredential);
+
+          User? authUser = userCredential.user;
+          String? emailUser = authUser!.email;
+          String? phoneNumber = authUser.phoneNumber;
+          CloudUserModel? currentCloudUser2;
+          CloudUserModel? currentCloudUser = await UserRepository.userRepository
+              .getCloudUserByEmail(emailUser.toString());
+          log(currentCloudUser.toString());
+          if (phoneNumber != null && emailUser == null) {
+            currentCloudUser2 = await UserRepository.userRepository
+                .getCloudUserByPhoneNumber(authUser.phoneNumber.toString());
+          }
+          if (currentCloudUser == null && currentCloudUser2 == null) {
+            currentCloudUser = await UserRepository.userRepository
+                .getCloudUserByPhoneNumber(authUser.phoneNumber.toString());
+            // Kiểm tra và gán giá trị mặc định nếu là null
+            String userName = authUser.displayName ?? 'Unknown';
+            String userEmail = authUser.email ?? 'Unknown';
+
+            await UserRepository.userRepository.createUser(
+              CloudUserModel.registerByGg(
+                userId: authUser.uid,
+                userName: userName,
+                email: userEmail,
+              ),
+            );
+          }
+          return userCredential;
+        } else {
+          throw FirebaseAuthException(
+            code: 'Facebook Login Failed',
+            message: 'The Facebook login was not successful.',
           );
         }
-        return userCredential;
-      } else {
-        throw FirebaseAuthException(
-          code: 'Facebook Login Failed',
-          message: 'The Facebook login was not successful.',
-        );
       }
     } on FirebaseAuthException catch (e) {
       // Handle Firebase authentication exceptions
