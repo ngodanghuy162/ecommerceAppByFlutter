9992e9bafa25bfb79534a2ebf35201578fe0944b
ngodanghuy162 <ngohuy658@gmail.com>
2023-12-03 06:52:22 +0700

    searching 2 add again

--- ecommerce_app_mobile/lib/features/shop/screens/search/search_sugges_screen.dart
+++ ecommerce_app_mobile/lib/features/shop/screens/search/search_sugges_screen.dart
@@ -37,3 +37,86 @@ class SearchingScreen extends StatelessWidget {
     );
   }
 }
+
+class SearchWidget extends StatefulWidget {
+  @override
+  _SearchWidgetState createState() => _SearchWidgetState();
+}
+
+class _SearchWidgetState extends State<SearchWidget> {
+  final List<String> suggestions = [
+    "Channel",
+    "CodingLab",
+    "CodingNepal",
+    "YouTube",
+    "YouTuber",
+    "YouTube Channel",
+    "Blogger",
+    "Bollywood",
+    "Vlogger",
+    "Vechiles",
+    "Facebook",
+    "Freelancer",
+    "Facebook Page",
+    "Designer",
+    "Developer",
+    "Web Designer",
+    "Web Developer",
+    "Login Form in HTML & CSS",
+    "How to learn HTML & CSS",
+    "How to learn JavaScript",
+    "How to became Freelancer",
+    "How to became Web Designer",
+    "How to start Gaming Channel",
+    "How to start YouTube Channel",
+    "What does HTML stands for?",
+    "What does CSS stands for?",
+  ];
+
+  TextEditingController _controller = TextEditingController();
+  List<String> _filteredSuggestions = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            controller: _controller,
+            onChanged: (value) {
+              filterSuggestions(value);
+            },
+            decoration: InputDecoration(
+              hintText: 'Search',
+              prefixIcon: Icon(Icons.search),
+            ),
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: _filteredSuggestions.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(_filteredSuggestions[index]),
+                onTap: () {
+                  // Handle item selection here
+                  print("Selected: ${_filteredSuggestions[index]}");
+                },
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  void filterSuggestions(String query) {
+    setState(() {
+      _filteredSuggestions = suggestions
+          .where((suggestion) =>
+              suggestion.toLowerCase().startsWith(query.toLowerCase()))
+          .toList();
+    });
+  }
+}
