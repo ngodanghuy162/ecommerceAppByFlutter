74d0d61069c8b7ad24698dff721bc1dd5126cf00
ngodanghuy162 <ngohuy658@gmail.com>
2023-12-13 10:04:02 +0700

    Merge branch 'khanh-paypal' into thuan-fromDat09

--- ecommerce_app_mobile/lib/repository/user_repository.dart
+++ ecommerce_app_mobile/lib/repository/user_repository.dart
@@ -1,4 +1,3 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
@@ -6,6 +5,8 @@ class UserRepository extends GetxController {
   static UserRepository get instance => Get.find();
 
   final _db = FirebaseFirestore.instance;
+
+ // final userCollection = FirebaseFirestore.instance.collection('Users');
 
   Future<String> getUserDocumentId(String id) async {
     final snapshot = await _db.collection('Users').doc(id).get();
